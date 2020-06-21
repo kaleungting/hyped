9649c019ef55aeca8ab26b8dcaf4e88351d44ec5
Ken Ting <kaleungting@gmail.com>
2020-06-21 16:26:31 -0400

    Added components and hover effect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./component/homepage/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 function App() {
   return (
     <div>
