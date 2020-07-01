0d0574c81005e3cfc68c14c428dca09c5a208c21
Ken Ting <kaleungting@gmail.com>
2020-07-01 09:51:11 -0400

    created cart-item component and finished cart dropdown

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
+import CartItem from "../cart-item/cart-item.component";
 import CustomButton from "../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = () => (
-  <div className="cart-dropdown">
-    <div className="cart-items" />
-    <CustomButton>GO TO CHECKOUT</CustomButton>
-  </div>
-);
+const CartDropdown = ({ cartItems }) => {
+  let total = 0;
+  cartItems.forEach(
+    (cartItem) => (total += cartItem.price * cartItem.quantity)
+  );
 
-export default CartDropdown;
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+      </div>
+      <div className="total">Total: ${total}</div>
+      <CustomButton>GO TO CHECKOUT</CustomButton>
+    </div>
+  );
+};
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+export default connect(mapStateToProps, null)(CartDropdown);
